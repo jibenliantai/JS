91cae582395735d1ad95b195dcf138bd58a32872
jibenliantai <y.renta0625@gmail.com>
2024-02-11 23:09:54 +0900

    課題

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ console.log(lists);
 
 const pullDownButton = document.getElementById("lists")
 console.log(pullDownButton)
+
+pullDownButton.addEventListener("mouseover",function() {
+    console.log("乗る");
+});
+
+pullDownButton.addEventListener("mouseout",function() {
+    console.log("外れる");
+});
+
+pullDownButton.addEventListener("click",function() {
+    console.log("クリック");
+});
