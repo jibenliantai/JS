17a1dad9cd3e0b1e725741f9828c63be8c569319
jibenliantai <y.renta0625@gmail.com>
2024-06-22 01:00:21 +0900

    条件分岐編

--- script.js
+++ script.js
@@ -4,14 +4,26 @@ console.log(lists);
 const pullDownButton = document.getElementById("lists")
 console.log(pullDownButton)
 
+const pullDownParents = document.getElementById("pull-down")
+
 pullDownButton.addEventListener("mouseover",function() {
-    pullDownButton.setAttribute("style","background-color:blue;")
+    this.setAttribute("style","background-color:blue;")
 });
 
 pullDownButton.addEventListener("mouseout",function() {
-    pullDownButton.setAttribute("style","background-color:red;")
+    this.setAttribute("style","background-color:red;")
 });
 
 pullDownButton.addEventListener("click",function() {
-    pullDownButton.setAttribute("style","background-color:green;")
+    this.setAttribute("style","background-color:green;")
+});
+
+pullDownButton.addEventListener("click",function() {
+    if (pullDownParents.getAttribute("style") == "display:block;"){
+        pullDownParents.removeAttribute("style","display:block;")
+        console.log("非表示")
+    } else {
+        pullDownParents.setAttribute("style","display:block;")
+        console.log("表示")
+    }
 });
